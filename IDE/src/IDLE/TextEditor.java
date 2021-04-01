0218493aae01f326b623faadd7c66817663d40de
Andrey <andreysansab@gmail.com>
2021-04-01 11:55:10 -0600

    [UPDATE] IDLE
    El editor muestra el numero de linea y resalta la linea actual.

--- IDE/src/IDLE/TextEditor.java
+++ IDE/src/IDLE/TextEditor.java
@@ -3,11 +3,14 @@ package IDLE;
 import javax.swing.*;
 import java.awt.*;
 
+
+
 public class TextEditor extends JPanel {
 
     private JPanel panel1;
     private JTextArea taEditor;
     private JScrollPane scrollpane;
+    private TextLineNumber tln;
 
     public TextEditor(){
         setLayout(new BorderLayout(0,0));
@@ -17,6 +20,12 @@ public class TextEditor extends JPanel {
 
         taEditor = new JTextArea();
         scrollpane.setViewportView(taEditor);
+
+        tln = new TextLineNumber(taEditor);
+        tln.setBorderGap(-1);
+        tln.setDigitAlignment(CENTER_ALIGNMENT);
+        tln.setCurrentLineForeground(Color.GREEN);
+        scrollpane.setRowHeaderView(tln);
 
     }
 
